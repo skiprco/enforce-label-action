58ab24fb52a7e113c0fe6f1587efedd7eb77af00
JensSkipr <62247524+JensSkipr@users.noreply.github.com>
2022-01-28 08:48:19 +0100

    Initial version

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run() {
+  try {
+    const labels = github.context!.payload!.pull_request!.labels;
+
+    enforceAnyLabels(labels);
+
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+function enforceAnyLabels(labels) {
+  const requiredLabelsAny: string = core.getInput('REQUIRED_LABELS_REGEX_ANY', {required: true});
+  const requiredLabelsRegexAny = new RegExp(requiredLabelsAny);
+  if (labels.length === 0) {
+    core.setFailed(`PR must have at least a single label to match`);
+  }
+  if (!labels.some((l: any) => validateLabelAgainstRegex(l.name, requiredLabelsRegexAny))) {
+    core.setFailed(`Please set one label for this PR which matches regex: ${requiredLabelsAny}`);
+  }
+}
+
+function validateLabelAgainstRegex(label: string, re: RegExp): boolean {
+  const match = re.test(label) 
+  console.log(`Label ${label} matches against regex ${re.source}: ${match}`)
+  return match
+}
+
+run();
