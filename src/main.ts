0e792ee6299327b94cabbb40ab5077d8b5ad7159
JensSkipr <62247524+JensSkipr@users.noreply.github.com>
2022-01-28 09:12:51 +0100

    Align input name

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,11 @@ async function run() {
   }
 }
 
-function enforceAnyLabels(labels) {
-  const requiredLabelsAny: string = getInputString('REQUIRED_LABELS_REGEX_ANY', '.*');
+function enforceAnyLabels(labels: string[]) {
+  const requiredLabelsAny: string = getInputString('REQUIRED_LABELS_REGEX_ANY');
   const requiredLabelsRegexAny = new RegExp(requiredLabelsAny);
   if (labels.find((l) => requiredLabelsRegexAny.test(l))) {
-    const requiredLabelsAnyDescription = getInputString('REQUIRED_LABELS_ANY_DESCRIPTION', `Please select one of the required labels for this PR: ${requiredLabelsAny}`);
-    core.setFailed(requiredLabelsAnyDescription);
+    core.setFailed(`Please set one label for this PR which matches regex: ${requiredLabelsAny}`);
   }
 }
 
