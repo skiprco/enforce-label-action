1534b6a26e658a7c03a8a827a785be61510e67f7
JensSkipr <62247524+JensSkipr@users.noreply.github.com>
2022-01-28 09:25:06 +0100

    Simplify code

--- src/main.ts
+++ src/main.ts
@@ -13,16 +13,11 @@ async function run() {
 }
 
 function enforceAnyLabels(labels: string[]) {
-  const requiredLabelsAny: string = getInputString('REQUIRED_LABELS_REGEX_ANY');
+  const requiredLabelsAny: string = core.getInput('REQUIRED_LABELS_REGEX_ANY', {required: true});
   const requiredLabelsRegexAny = new RegExp(requiredLabelsAny);
   if (labels.find((l) => requiredLabelsRegexAny.test(l))) {
     core.setFailed(`Please set one label for this PR which matches regex: ${requiredLabelsAny}`);
   }
-}
-
-function getInputString(name, defaultValue): string {
-  const rawInput = core.getInput(name, {required: false});
-  return rawInput !== '' ? rawInput : defaultValue;
 }
 
 run();
