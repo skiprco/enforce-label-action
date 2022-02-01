5d9c8ded8767832a84a80fd4bfebe15bad09d9f8
JensSkipr <62247524+JensSkipr@users.noreply.github.com>
2022-02-01 09:12:40 +0100

    Convert to Typescript action

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:14.14.0-slim
-
-COPY . .
-
-RUN yarn install
-RUN node_modules/typescript/bin/tsc
-
-ENTRYPOINT ["node", "/lib/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,17 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {PullRequestEvent} from '@octokit/webhooks-types'
+import {enforceAnyLabels} from './labels'
 
-async function run() {
+async function run(): Promise<void> {
   try {
-    const labels = github.context!.payload!.pull_request!.labels;
-
-    enforceAnyLabels(labels);
-
-  } catch (error: any) {
-    core.setFailed(error.message);
+    const requiredLabelsAny: string = core.getInput('REQUIRED_LABELS_REGEX_ANY')
+    const labels = (github.context.payload as PullRequestEvent).pull_request
+      .labels
+    enforceAnyLabels(requiredLabelsAny, labels)
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-function enforceAnyLabels(labels) {
-  const requiredLabelsAny: string = core.getInput('REQUIRED_LABELS_REGEX_ANY', {required: true});
-  const requiredLabelsRegexAny = new RegExp(requiredLabelsAny);
-  if (labels.length === 0) {
-    core.setFailed(`PR must have at least a single label to match`);
-  }
-  if (!labels.some((l: any) => validateLabelAgainstRegex(l.name, requiredLabelsRegexAny))) {
-    core.setFailed(`Please set one label for this PR which matches regex: ${requiredLabelsAny}`);
-  }
-}
-
-function validateLabelAgainstRegex(label: string, re: RegExp): boolean {
-  const match = re.test(label) 
-  console.log(`Label ${label} matches against regex ${re.source}: ${match}`)
-  return match
-}
-
-run();
+run()
